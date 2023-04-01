bfaf51d93a502c0cc2214bc7556cd9c9ff9ca879
lucas.gentile95@gmail.com <lucas.gentile95@gmail.com>
2023-03-31 22:55:49 -0300

    Upando meu desafio de código

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        ContaTerminal conta = new ContaTerminal();
+
+        System.out.println("Digite o número da Agencia: ");
+        conta.setAgencia(scan.next());
+        System.out.println("Digite o número da sua Conta: ");
+        conta.setNumero(scan.nextInt());
+        System.out.println("Informe o saldo da sua Conta: ");
+        conta.setSaldo(scan.nextDouble());
+        System.out.println("Digite o seu nome: ");
+        scan.nextLine();
+        conta.setNomeCliente(scan.next());
+
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, " +
+                "conta %d e seu saldo já está disponível para saque.", conta.getNomeCliente(), conta.getAgencia(), conta.getNumero());
+
+    }
+}
